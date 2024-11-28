dc47cb8b26fed90a2becd5822320d19975556cce
Gabriel <ga.salandra@gmail.com>
2024-11-28 14:11:29 -0600

    comment circuit

--- scripts/circuit.sh
+++ scripts/circuit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-TO_EMAIL="ga.salandra@gmail.com"
-SUBJECT="CodeDeploy AfterInstall Notification"
-BODY="The AfterInstall phase of your CodeDeploy deployment has completed."
+# TO_EMAIL="ga.salandra@gmail.com"
+# SUBJECT="CodeDeploy AfterInstall Notification"
+# BODY="The AfterInstall phase of your CodeDeploy deployment has completed."
 
-echo "$BODY" | mail -s "$SUBJECT" "$TO_EMAIL"
+# echo "$BODY" | mail -s "$SUBJECT" "$TO_EMAIL"
 
-if [ $? -eq 0 ]; then
-    echo "Email sent successfully"
-    exit 0
-else
-    echo "Failed to send email"
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "Email sent successfully"
+#     exit 0
+# else
+#     echo "Failed to send email"
+#     exit 1
+# fi
