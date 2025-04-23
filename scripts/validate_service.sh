63f2eba1a250fae9b2bf8fd934d6a85dc221ee89
gabsalk <gabsalk@amazon.com>
2025-04-22 19:27:30 -0600

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if systemctl is-active --quiet httpd; then
+    systemctl stop httpd
+    echo "Apache web server stopped"
+else
+    echo "Apache web server is not running"
+fi
+
 if [ -d /var/www/html ]; then
     rm -rf /var/www/html/*
     echo "Cleaned /var/www/html directory"
